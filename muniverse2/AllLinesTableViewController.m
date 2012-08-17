246a7a40aa1a8a41f729feb34026889d8f7e3bd4
Jamison Wieser <jamison@fattrash.com>
2012-08-17 07:52:19 -0700

    Created a placeholder nearby view controller, added to stop detail screen

--- muniverse2/AllLinesTableViewController.m
+++ muniverse2/AllLinesTableViewController.m
@@ -44,10 +44,6 @@ typedef enum {
     
     AppDelegate *app = [[UIApplication sharedApplication] delegate];
     self.moc = app.managedObjectContext;
-    
-    
-    // Jamison was seeing if he'd figured out how to change the navigationbar color
-    // and set colors correctly.
 
     self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:80/255.0f green:109/255.0f blue:131/255.0f alpha:1];
 
