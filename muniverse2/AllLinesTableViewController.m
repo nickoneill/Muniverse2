b8c21d078d7934010b583e4bc5ffb8aaf20bbb9b
Nick O'Neill <nick.oneill@gmail.com>
2012-08-26 19:58:27 -0700

    Remove some boilerplate code

--- muniverse2/AllLinesTableViewController.m
+++ muniverse2/AllLinesTableViewController.m
@@ -51,12 +51,6 @@ typedef enum {
     if (![[self frc] performFetch:&error]) {
         NSLog(@"whoops with Lines frc: %@",error);
     }
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (NSFetchedResultsController *)frc {
