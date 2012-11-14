9de05a04dbc234a0c3b16a63d0b7254b031b2167
Nick O'Neill <nick.oneill@gmail.com>
2012-11-14 12:48:05 -0800

    Added favorite checking for stops.

--- muniverse2/Stop+Favorite.m
+++ muniverse2/Stop+Favorite.m
@@ -7,13 +7,29 @@
 //
 
 #import "Stop+Favorite.h"
+#import "AppDelegate.h"
 
 // we maintain a category for this favorite method so we can regenerate the managed objects without loss of code
 @implementation Stop (Favorite)
 
 - (BOOL)isFavorite
 {
-    return YES;
+    AppDelegate *app = [[UIApplication sharedApplication] delegate];
+    
+    NSFetchRequest *fetch = [NSFetchRequest fetchRequestWithEntityName:@"Favorite"];
+    
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"%@ == %K",self,@"stop"];
+    
+    [fetch setPredicate:pred];
+    
+    NSError *err;
+    int favorites = [app.managedObjectContext countForFetchRequest:fetch error:&err];
+    
+    if (favorites > 0) {
+        return YES;
+    }
+    
+    return NO;
 }
 
 @end
