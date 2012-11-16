79f8e739c2d28b4e0017c24607c556b015c07d7c
Nick O'Neill <nick.oneill@gmail.com>
2012-11-15 17:02:36 -0800

    Removed code.

--- muniverse2/FavoritesViewController.m
+++ muniverse2/FavoritesViewController.m
@@ -315,17 +315,6 @@
 
 #pragma mark - Table view delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
-
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     // set our detail stop info
